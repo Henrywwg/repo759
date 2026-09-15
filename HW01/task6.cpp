d8ef6d12c62c0621f13a0221378ae4071091ef18
Henry Wysong-Grass <henryjwg@gmail.com>
2026-09-14 23:05:50 -0500

    task6.cpp

---
+++ HW01/task6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Read in cmdline value
+
+int main(int argc, char* argv[]) {
+    int n = 0;
+    if (argc > 1) 
+        n = atoi(argv[1]);
+    else 
+        return 1;
+
+    for (int i = 1; i <= n; i++) 
+        printf("%d ", i);
+    printf("\n");
+
+    for (int i = n; i >= 1; i--) 
+        cout << i << endl;
+    cout << endl;
+    
+    return 0;
+}
